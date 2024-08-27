363893f267c5456af20a4c764780e3a469e535f9
Philippe Yong <philippeyong927@gmail.com>
2024-08-27 15:38:37 +1000

    Fix import bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import Navbar from "./components/Navbar";
